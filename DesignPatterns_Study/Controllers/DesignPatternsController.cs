659019e2708c27a52761e19d715a835a57f57fcb
黃振倫 <s52076663135@gmail.com>
2016-03-01 22:49:55 +0800

    1.物件導向目的及好處

--- DesignPatterns_Study/Controllers/DesignPatternsController.cs
+++ DesignPatterns_Study/Controllers/DesignPatternsController.cs
@@ -11,7 +11,31 @@ namespace DesignPatterns_Study.Controllers
         // GET: DesignPatterns
         public ActionResult Index()
         {
+            //物件導向目的
+            /*
+            第一，要改，只需要改要改的東西，代表可維護
+            第二，可以後來重複使用，代表可複用
+            第三，如果要加條件，只需要在另外增加就可以，代表可擴展
+            第四，可透過條件改變一下，就可以滿足需求，此是靈活性好
+            
+            //物件導向好處
+            過去寫的程式，每次都可能導致重頭來過，原因就是少於上述
+            ，所以開始透過封裝、繼承、多型把程式耦和度降低
+
+            */
             return View();
         }
+
+        /// <summary>
+        /// 01-簡單工廠模式
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SimpleFactoryPattern()
+        {
+
+
+            return View();
+        }
+
     }
 }
